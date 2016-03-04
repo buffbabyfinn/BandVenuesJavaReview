8d0e3477b2406960cff8fa0f1cd4a7f93f69145f
Megan <mefayer@gmail.com>
2016-03-03 16:15:12 -0800

    change file names for review project, update spark for checkboxes, and change db rules

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -4,19 +4,17 @@ import org.sql2o.*;
 public class DatabaseRule extends ExternalResource {
 
   protected void before() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/library_test", null, null);
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/band_venues_test", null, null);
    }
 
   protected void after() {
     try(Connection con = DB.sql2o.open()) {
-      String deleteBooksQuery = "DELETE FROM books *;";
-      String deleteAuthorsQuery = "DELETE FROM authors *;";
-      String deletePatronsQuery = "DELETE FROM patrons *;";
-      String deleteCheckoutQuery = "DELETE FROM checkout *;";
-      con.createQuery(deleteBooksQuery).executeUpdate();
-      con.createQuery(deleteCheckoutQuery).executeUpdate();
-      con.createQuery(deleteAuthorsQuery).executeUpdate();
-      con.createQuery(deletePatronsQuery).executeUpdate();
+      String deleteBandsQuery = "DELETE FROM bands *;";
+      String deleteVenuesQuery = "DELETE FROM venues *;";
+      String deleteBandsVenuesQuery = "DELETE FROM bands_venues *;";
+      con.createQuery(deleteBandsQuery).executeUpdate();
+      con.createQuery(deleteVenuesQuery).executeUpdate();
+      con.createQuery(deleteBandsVenuesQuery).executeUpdate();
     }
 }
 }
